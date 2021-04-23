2357aff4d68d12ab53bb2a8151921c3d75680e26
Amelia Thompson <ameliasthompson32@gmail.com>
2021-04-23 15:09:08 -0400

    Implemented select range

--- Project2/Query.java
+++ Project2/Query.java
@@ -34,4 +34,38 @@ public class Query {
             System.out.println("");
         }
     }
+
+    /**
+     * Select rows that fall in the exclusive range of values provided.
+     * @param l lower bounds (exclusive)
+     * @param u upper bounds (exclusive)
+     */
+    public static void selectRange(int l, int u) {
+        DBUtil.getAndResetReadCounter(); // Make sure read counter is 0
+        long start = new Date().getTime(); // Start timer
+
+        ArrayIndex aIndex = IndexManager.getArrayIndex();
+        if (aIndex == null) {
+            // Table scan
+            TableScan.findRecordsRange(l, u);
+
+            long finish = new Date().getTime();
+
+            System.out.println("Table scan was used.");
+            System.out.println("Query took " + (finish - start) + " ms to complete.");
+            System.out.println("Query read " + DBUtil.getAndResetReadCounter() + " blocks.");
+            System.out.println("");
+
+        } else {
+            // Hash index used
+            aIndex.findRecordsRange(l, u);
+
+            long finish = new Date().getTime();
+
+            System.out.println("The array index was used.");
+            System.out.println("Query took " + (finish - start) + " ms to complete.");
+            System.out.println("Query read " + DBUtil.getAndResetReadCounter() + " blocks.");
+            System.out.println("");
+        }
+    }
 }
