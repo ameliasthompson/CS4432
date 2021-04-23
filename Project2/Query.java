5af1c572feb41a2761c222b56d10b867393ec1d4
Amelia Thompson <ameliasthompson32@gmail.com>
2021-04-23 15:15:38 -0400

    Added inequals query

--- Project2/Query.java
+++ Project2/Query.java
@@ -68,4 +68,22 @@ public class Query {
             System.out.println("");
         }
     }
+
+    /**
+     * Select rows where v does not match RandomV
+     * @param v value
+     */
+    public static void selectInequal(int v) {
+        DBUtil.getAndResetReadCounter(); // Make sure read counter is 0
+        long start = new Date().getTime(); // Start timer
+
+        TableScan.findRecordsInequal(v);
+
+        long finish = new Date().getTime();
+
+        System.out.println("Table scan was used.");
+        System.out.println("Query took " + (finish - start) + " ms to complete.");
+        System.out.println("Query read " + DBUtil.getAndResetReadCounter() + " blocks.");
+        System.out.println("");
+    }
 }
