f10d29d28b702f7a8f97ac367d0839aaf24944d4
Amelia Thompson <ameliasthompson32@gmail.com>
2021-04-23 11:57:54 -0400

    Initial DBUtil getRandomV implementation

---
+++ Project2/DBUtil.java
@@ -0,0 +1,49 @@
+import java.nio.file.Paths;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class DBUtil {
+
+    private static final int NUM_RECORDS_IN_BLOCK = 100;
+    private static final int RECORD_SIZE = 40;
+    private static final int RANDOM_V_OFFSET = 33;
+    private static final int RANDOM_V_SIZE = 4;
+
+    /**
+     * Get the RandomV value of a database record.
+     * @param rid record id
+     * @return RandomV value
+     */
+    public static int getRandomV(int rid) {
+        // First figure out which block the record is in:
+        rid--; // Record id is one indexed, but it's easier to find with zero index.
+        if (rid < 0) { return -1; }
+        int bid = (rid / NUM_RECORDS_IN_BLOCK) + 1; // Have to adjust because block is one indexed
+
+        // Read the block file into memory:
+        Path p = Paths.get("Project2Dataset/F" + Integer.toString(bid) + ".txt");
+        byte[] file;
+        try {
+            file = Files.readAllBytes(p);
+        } catch (IOException e) {
+            System.out.println("Error opening block " + bid + " file.");
+            e.printStackTrace();
+            return -1;
+        }
+
+        // Isolate the record RandomV we're interested in:
+        byte[] value = new byte[RANDOM_V_SIZE];
+        try {
+            int srcPos = (rid % NUM_RECORDS_IN_BLOCK) * RECORD_SIZE + RANDOM_V_OFFSET;
+            System.arraycopy(file, srcPos, value, 0, RANDOM_V_SIZE);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Record " + rid + " could not be copied from block " + bid + " file.");
+            e.printStackTrace();
+            return -1;
+        }
+
+        return Integer.parseInt(new String(value));
+    }
+}
