54fcfcf2e64da3ee9390ae2ce3d797836a9c500d
Amelia Thompson <ameliasthompson32@gmail.com>
2021-04-23 15:08:45 -0400

    Added records in range to table scan

--- Project2/TableScan.java
+++ Project2/TableScan.java
@@ -32,4 +32,40 @@ public class TableScan {
             return null;
         }
     }
+
+    /**
+     * Use a full table scan to find all records between l and u (exclusive)
+     * @param l lower bound (exclusive)
+     * @param u upper bound (exclusive)
+     * @return
+     */
+    public static int[] findRecordsRange(int l, int u) {
+        if (l >= u) {
+            System.out.println("Error: Attempted to find record with invalid RandomV range.");
+            return null;
+        }
+
+        ArrayList<Integer> records = new ArrayList<Integer>();
+        for (int i = 1; i <= DBUtil.NUM_RECORDS; i++) {
+            int value = DBUtil.getRandomV(i);
+            if (value > l && value < u) {
+                records.add(i);
+                System.out.println(DBUtil.getRecordText(i));
+            }
+        }
+
+        if (records.size() > 0) {
+            // Because we can't use int in array lists, we have to jump through
+            // some hoops to return an int[] here instead of just using toArray()
+            int[] tmp = new int[records.size()];
+            for (int i = 0; i < records.size(); i++) {
+                tmp[i] = records.get(i);
+            }
+
+            return tmp;
+        } else {
+            // Return null if there are no records
+            return null;
+        }
+    }
 }
